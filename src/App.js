72a0f8e67f1f5de79a5f2f8c75fa45e7e1ce264b
Shivam <shivam151198@gmail.com>
2022-12-09 10:01:29 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+      <h1>Ecomm Project</h1>
+      <button>Normal button</button>
+      <Button >bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
